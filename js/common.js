d8812b1bdf17a0d53e4e874ae7bb3abf9aff8cff
CoviDeal <eyukun417@users.noreply.github.com>
2020-10-16 10:24:52 +0800

    Update

--- js/common.js
+++ js/common.js
@@ -5,21 +5,21 @@ Student Name: Ng Jun Zhi
 Student ID: B1802197
 **/		
 
-// Users
+// Users initialised values
 var users = {
-	manager: {
-		id: '1',
-		username: 'admin',
-		password: 'admin123',
-		name: 'admin',
-		position: 'manager',
-		patientType: 'null',
-		symptoms: 'null',
-		centreID: '1'
-	},
+		manager: {
+			id: 1,
+			username: 'admin',
+			password: 'admin123',
+			name: 'admin',
+			position: 'manager',
+			patientType: 'null',
+			symptoms: 'null',
+			centreID: 1
+		},
 
 	manager1: {
-		id: '3',
+		id: 2,
 		username: 'adminadminadmin',
 		password: 'adminadminadmin123',
 		name: 'adminadminadmin',
@@ -30,18 +30,18 @@ var users = {
 	},
 
 	tester: {
-		id: '4',
+		id: 3,
 		username: 'tester',
 		password: 'tester123',
 		name: 'tester',
 		position: 'tester',
 		patientType: 'null',
 		symptoms: 'null',
-		centreID: '1'
+		centreID: 1
 	},
 
 	patient: {
-		id: '5',
+		id: 4,
 		username: 'patient',
 		password: 'patient123',
 		name: 'patient',
@@ -52,16 +52,22 @@ var users = {
 	},
 	
 	officer: {
-		id: '6',
+		id: 5,
 		username: 'officer',
 		password: 'officer123',
 		name: 'officer',
 		position: 'officer',
 		patientType: 'null',
 		symptoms: 'null',
-		centreID: '1'		
+		centreID: 1		
 	}	
 }
+
+// current id
+var currentCentreID = 2;
+var currentUserID = 6;
+
+
 
 // Users array
 var allUsers = [];
@@ -71,22 +77,54 @@ for (var user in users){
 	allUsers.push(users[user]);
 }
 
+// current user info
 var currentUser = {
-	id: undefined,
-	username: undefined,
-	password: undefined,
-	name: undefined,
-	position: undefined,
-	patientType: undefined,
-	symptoms: undefined,
-	centreID: undefined
+	id: 'null',
+	username: 'null',
+	password: 'null',
+	name: 'null',
+	position: 'null',
+	patientType: 'null',
+	symptoms: 'null',
+	centreID: 'null'
 }
 
+// create centre
+var createCentre = {
+	centreID: 'null',
+	centreName: 'null',
+	address: 'null',
+	id: 'null'
+}
+
+// Centres initialised values
+var centres = {
+	kl: {
+		centreID: 1,
+		centreName: 'kl',
+		address: '123, Jalan KL, KL',
+		id: 1
+	}
+}
+
+// Centres array
+var allCentres = [];
+
+// add in the centres
+for (var centre in centres){
+	allCentres.push(centres[centre]);
+}
+
+
+// function to determine which function going
 function common(){
 	let action = document.getElementById('action').value;
 	switch(action){
 		case 'login':
 			loginFormSubmitted();
+			break;
+		case 'registerTestCentre':
+			registerCentreFormSubmitted();
 			break;
 		default:
 			alert(action);
@@ -94,12 +132,18 @@ function common(){
 	}
 }
 
+// login form
 function loginFormSubmitted(){
 	event.preventDefault();
-	let loginFormData = captureLoginFormData();
+	
+	// get data from login form
+	let formData = captureLoginFormData();
 	let login = false;
-	for (var i = 0; i < allUsers.length; i++){
-		if (loginFormData['username'] == allUsers[i].username && loginFormData['password'] == allUsers[i].password){
+	
+	// verify the username and password
+	for (let i = 0; i < allUsers.length; i++){
+		if (formData['username'] == allUsers[i].username && formData['password'] == allUsers[i].password){
+			// set the data to current user
 			currentUser['id'] = allUsers[i].id;
 			currentUser['username'] = allUsers[i].username;
 			currentUser['password'] = allUsers[i].password;
@@ -109,6 +153,8 @@ function loginFormSubmitted(){
 			currentUser['symptoms'] = allUsers[i].symptoms;
 			currentUser['centreID'] = allUsers[i].centreID;
 			login = true;
+			
+			// navigate the user to specific pages with his/her position
 			switch (currentUser['position']){
 				case 'manager':
 					if (allUsers[i].centreID == 'null'){
@@ -134,20 +180,130 @@ function loginFormSubmitted(){
 			}
 		}
 	}
+	// invalid login
 	if (login == false){
 		alert('Invalid Username or Password !');
 		window.location.href = "index.html";
 	}
 }
 
+// capture login form data
 function captureLoginFormData(){
-	let loginFormData = {};
-	loginFormData["username"] = document.getElementById("username").value;
-	loginFormData["password"] = document.getElementById("password").value;
-	return loginFormData;
+	let formData = {};
+	// get data from login form
+	formData["username"] = document.getElementById("username").value;
+	formData["password"] = document.getElementById("password").value;
+	return formData;
 }
 
 
+// register test centre form
+function registerCentreFormSubmitted(){
+	event.preventDefault();
+	
+	// get data from register centre form
+	let formData = captureRegisterCentreFormData();
+	let register = true;
+	
+	// remove the message created before
+	if (document.getElementById("errorMsg") != null){
+		document.getElementById("errorMsg").remove();
+	}
+	for (let i = 0; i < allCentres.length; i++){
+		if (formData['centreName'] == allCentres[i].centreName){
+			register = false; 
+			
+			// append the failure message
+			let aNode = document.createElement("a");
+			aNode.setAttribute("class", "close");
+			aNode.setAttribute("data-dismiss", "alert");
+			aNode.setAttribute("aria-label", "close");
+			aNode.setAttribute("href", "#");
+			aNode.innerHTML = "&times;";
+			
+			let strongNode = document.createElement("strong");
+			let textNode = document.createTextNode("Cannot add ! " + allCentres[i].centreName + " (Test Centre) has already existed.");
+			strongNode.appendChild(textNode);
+						
+			let divNode = document.createElement("div");
+			divNode.setAttribute("class", "alert alert-danger alert-dismissible fade show");
+			divNode.appendChild(aNode);
+			divNode.appendChild(strongNode);
+			divNode.setAttribute("id", "errorMsg");
+			
+			document.getElementById("error").appendChild(divNode);
+		}
+		break;
+	}
+	if (register == true){
+		// create centre and push into allCentres
+		createCentre = {
+			centreID: currentCentreID++,
+			centreName: formData['centreName'],
+			address: formData['address'],
+			id: currentUser['id']
+		}
+		allCentres.push(createCentre);
+		
+		// set centreID to currentUser
+		currentUser['centreID'] = createCentre['centreID'];
+		
+		// append the success message
+		let aNode = document.createElement("a");
+		aNode.setAttribute("class", "close");
+		aNode.setAttribute("data-dismiss", "alert");
+		aNode.setAttribute("aria-label", "close");
+		aNode.setAttribute("href", "#");
+		aNode.innerHTML = "&times;";
+		
+		let strongNode = document.createElement("strong");
+		let textNode = document.createTextNode("New test centre (" + createCentre['centreName'] + ") has been added successfully!");
+		strongNode.appendChild(textNode);
+		
+		let divNode = document.createElement("div");
+		divNode.setAttribute("class", "alert alert-success alert-dismissible fade show");
+		divNode.appendChild(aNode);
+		divNode.appendChild(strongNode);
+		divNode.setAttribute("id", "errorMsg");
+		
+		document.getElementById("error").appendChild(divNode);
+		
+		// update the navigation details
+		let nav = document.getElementsByTagName("a");
+		nav[1].setAttribute("class", "nav-link text-secondary");
+		nav[1].setAttribute("href", "#");
+		nav[1].setAttribute("title", "You are owning a test centre currently !");
+
+		nav[2].setAttribute("class", "nav-link");
+		nav[2].setAttribute("href", "RecordTester.html");
+		nav[2].removeAttribute("title");
+
+		nav[3].setAttribute("class", "nav-link");
+		nav[3].setAttribute("href", "ManageTestKit.html");
+		nav[3].removeAttribute("title");
+		
+		btn1.setAttribute("class", "btn btn-dark");
+		btn1.setAttribute("data-target", "#");
+		btn1.setAttribute("title", "You cannot register more than one test centre !");
+
+	}
+	
+}
+
+function captureRegisterCentreFormData(){
+	// get data from register centre form
+	let formData = {};
+	formData['centreName'] = document.getElementById('centreName').value;
+	formData['address'] = document.getElementById('address').value;
+	return formData;
+}
+
+// hide the modal once the register centre form submitted
+$(document).ready(function() {
+   $('#registerCentreForm').submit(function() {
+		$("#registerCentreModal").modal("hide");
+	});
+});
 
 // validation function for modal
 (function() {
@@ -168,8 +324,6 @@ window.addEventListener('load', function() {
 	});
 }, false);
 })();
-
-
 
 
 /**
